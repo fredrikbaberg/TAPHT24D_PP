b2493feb69954d3a55914c2c4b0decdb049385ae
Fredrik Baberg <fredrik.baberg@gmail.com>
2025-03-02 22:53:01 +0100

    Korta ner kommentarer för chest

--- src/chest.py
+++ src/chest.py
@@ -1,10 +1,11 @@
 """Kista som kan finnas på kartan."""
+# pylint: disable=too-few-public-methods
 
 class Chest:
     """ Kista, kräver nyckel för att öppnas. """
-    # TODO: K. Nycklar och kistor - slumpa minst en nyckel och lika många kistor på spelplanen.\n
-    # När man går på en ruta med en nyckel plockar man upp den i sitt inventory. Om man kommer\n
-    # till en kista och har minst en nyckel, öppnar man kistan och plockar upp en skatt som är\n
-    # värd 100 poäng. (Nyckeln är förbrukad.)
-    def __init__(self):
-        super().__init__(name='chest', value=0, symbol='💼')
+    def __init__(self, symbol="C"): # Tecknet 💼 tar för mycket plats, använder C för "Chest".
+        self.name = 'chest'
+        self.symbol = symbol
+
+    def __str__(self):
+        return self.symbol
